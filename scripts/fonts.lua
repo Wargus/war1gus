90e3756b7c11033728fdbbc9ae6ab74b0a469e65
Tim Felgentreff <timfelgentreff@gmail.com>
2013-05-01 19:05:08 +0200

    reflect directory rename in scripts

--- scripts/fonts.lua
+++ scripts/fonts.lua
@@ -28,11 +28,11 @@
 --
 --      $Id$
 
-CFont:New("small", CGraphic:New("3rd/fonts/small.png", 7, 6))
-CFont:New("game", CGraphic:New("3rd/fonts/game.png", 13, 14))
-CFont:New("large", CGraphic:New("3rd/fonts/large.png", 17, 17))
-CFont:New("small-title", CGraphic:New("3rd/fonts/small_episode_titles.png", 32, 35))
-CFont:New("large-title", CGraphic:New("3rd/fonts/large_episode_titles.png", 52, 50))
+CFont:New("small", CGraphic:New("contrib/fonts/small.png", 7, 6))
+CFont:New("game", CGraphic:New("contrib/fonts/game.png", 13, 14))
+CFont:New("large", CGraphic:New("contrib/fonts/large.png", 17, 17))
+CFont:New("small-title", CGraphic:New("contrib/fonts/small_episode_titles.png", 32, 35))
+CFont:New("large-title", CGraphic:New("contrib/fonts/large_episode_titles.png", 52, 50))
 
 --[[
 DefineFont({
