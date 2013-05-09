7df077eb57925dc170f18267262f0c5df838cba8
Tim Felgentreff <timfelgentreff@gmail.com>
2013-05-09 13:52:23 +0200

    fix typo and position the rescue point correctly

--- scripts/tilesets/dungeon.lua
+++ scripts/tilesets/dungeon.lua
@@ -39,10 +39,10 @@ DefineTileset("name", "Dungeon",
   -- Slots descriptions
   "slots",
 	{
-	"solid", {"land" -- "wall"
+	"solid", {"land", -- "wall"
           {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
 	   10, 11, 12, 13, 14, 15}},   -- 000
-	"solid", {"land" -- "wall"
+	"solid", {"land", -- "wall"
           {16, 17, 18, 19, 20, 31, 22, 23, 24, 25,
 	   26, 27, 28, 29, 30, 31}}, -- water                    -- 010
 	"solid", {"rock", "unpassable",
