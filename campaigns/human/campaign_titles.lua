e9d24bffb2bff96a485b55e9d30f276da7596329
Tim Felgentreff <timfelgentreff@gmail.com>
2013-05-09 11:34:18 +0200

    add campaign info

---
+++ campaigns/human/campaign_titles.lua
@@ -0,0 +1,13 @@
+campaign_titles = {
+   "I. Intro",
+   "II. Grand Hamlet",
+   "III. Kyross",
+   "IV. Dead Mines",
+   "V. Elwynn Forest",
+   "VI. Northshire Abbey",
+   "VII. Sunnyglade",
+   "VIII. Medivh's Tower",
+   "IX. Black Morass",
+   "X. Temple of the Damned",
+   "XI. Blackard & Stonard",
+   "XII. Black Rock Spire"}
---
+++ campaigns/orc/campaign_titles.lua
@@ -0,0 +1,13 @@
+campaign_titles = {
+   "I. Intro",
+   "II. Kyross",
+   "III. Grand Hamlet",
+   "IV. Dead Mines",
+   "V. Red Ridge Mountains",
+   "VI. Sunnyglade",
+   "VII. Black Morass",
+   "VIII. Northshire Abbey",
+   "IX. (unknown)",
+   "X. Elwynn Forest",
+   "XI. Goldshire & Moonbrook",
+   "XII. Stormwind Keep"}
