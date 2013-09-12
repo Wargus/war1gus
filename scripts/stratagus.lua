6e381dc26a4b4ab363100a90211a7cacff8531a1
Tim Felgentreff <timfelgentreff@gmail.com>
2013-09-12 15:45:27 +0200

    Don't die if on windows the intro video has not been extracted. Also, let's not play the intro video every time.

--- scripts/stratagus.lua
+++ scripts/stratagus.lua
@@ -38,6 +38,15 @@ war1gus.Homepage = "https://launchpad.net/war1gus"
 war1gus.Licence = "GPL v2"
 war1gus.Copyright = "Copyright (c) 1998-2010 by The Stratagus Project"
 
+function file_exists(path, name)
+   for i,f in ipairs(ListFilesInDirectory(path)) do
+      if f == name then
+	 return true
+      end
+   end
+   return false
+end
+
 InitFuncs = {}
 function InitFuncs:add(f)
   table.insert(self, f)
@@ -60,38 +69,6 @@ end
 --  Note, some of those values are overridden by user preferences,
 --  see ~/.stratagus/preferences1.scripts
 --  and ~/.stratagus/gamename/preferences2.scripts
-
---  Enter your default title screen.
-
-SetTitleScreens(
-  {Image = "ui/logo.png",
-   Music = "sounds/logo.wav",
-   Timeout = 3},
-  {Image = "videos/hintro1.avi",
-   Music = "sounds/intro_1.wav",
-   Iterations = 1},
-  {Image = "videos/hintro2.avi",
-   Music = "sounds/intro_2.wav",
-   Iterations = 10},
-  {Image = "videos/ointro1.avi",
-   Music = "sounds/intro_3.wav",
-   Iterations = 1},
-  {Image = "videos/ointro2.avi",
-   Iterations = 10},
-  {Image = "videos/ointro3.avi",
-   Music = "sounds/intro_door.wav",
-   Iterations = 1},
-  {Image = "videos/cave1.avi",
-   Music = "sounds/intro_4.wav",
-   Iterations = 1},
-  {Image = "videos/cave2.avi",
-   Iterations = 3},
-  {Image = "videos/cave3.avi",
-   Music = "sounds/intro_5.wav",
-   Iterations = 1},
-  {Image = "videos/title.avi",
-   Iterations = 1}
-)
 
 
 --  Enter your menu music.
@@ -389,6 +366,7 @@ if (preferences == nil) then
     MaxOpenGLTexture = 0,
     CampaignOrc = 1,
     CampaignHuman = 1,
+    PlayIntro = true
   }
 end
 
@@ -407,6 +385,47 @@ SetMusicEnabled(preferences.MusicEnabled)
 SetMusicVolume(preferences.MusicVolume)
 SetTranslationsFiles(preferences.StratagusTranslation, preferences.GameTranslation)
 SetGrabMouse(preferences.GrabMouse)
+
+
+if file_exists("videos", "hintro1.avi") and preferences.PlayIntro then
+   SetTitleScreens(
+      {Image = "ui/logo.png",
+       Music = "sounds/logo.wav",
+       Timeout = 3},
+      {Image = "videos/hintro1.avi",
+       Music = "sounds/intro_1.wav",
+       Iterations = 1},
+      {Image = "videos/hintro2.avi",
+       Music = "sounds/intro_2.wav",
+       Iterations = 10},
+      {Image = "videos/ointro1.avi",
+       Music = "sounds/intro_3.wav",
+       Iterations = 1},
+      {Image = "videos/ointro2.avi",
+       Iterations = 10},
+      {Image = "videos/ointro3.avi",
+       Music = "sounds/intro_door.wav",
+       Iterations = 1},
+      {Image = "videos/cave1.avi",
+       Music = "sounds/intro_4.wav",
+       Iterations = 1},
+      {Image = "videos/cave2.avi",
+       Iterations = 3},
+      {Image = "videos/cave3.avi",
+       Music = "sounds/intro_5.wav",
+       Iterations = 1},
+      {Image = "videos/title.avi",
+       Iterations = 1}
+   )
+   preferences.PlayIntro = false
+else
+   SetTitleScreens(
+      {Image = "ui/logo.png",
+       Music = "sounds/logo.wav",
+       Timeout = 3}
+   )
+end
+
 
 --- Uses Stratagus Library path!
 Load("scripts/wc1.lua")
