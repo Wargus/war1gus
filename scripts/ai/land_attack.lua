08a2f0bd7ebedf80a12cf3396d612f9b625bb180
Tim Felgentreff <timfelgentreff@gmail.com>
2015-12-14 00:07:24 +0100

    try generating roads over and over, this way we'll eventually have a grid

--- scripts/ai/land_attack.lua
+++ scripts/ai/land_attack.lua
@@ -31,6 +31,112 @@
 -- defines a land attack ai. the sleep_factor is a factor to all
 -- a sleep statements, to scale down difficulty
 -- smaller sleep_factors mean faster AI
+
+-- a function to just generate roads. this gives the ai player an advantage, but what the heck
+function GenerateRoads(do_second, do_third)
+    local first_roads = {
+                            {{ 0,-1}, { 0,-2}, { 0,-3}, { 0,-4}, { 0,-5}, { 0,-6}, { 0,-7}, { 0,-8}, { 0,-9}},
+                            {{-1,-1}, {-2,-1}, {-3,-1}, {-4,-1}, {-5,-1}, {-6,-1}, {-7,-1}, {-8,-1}, {-9,-1}},
+                            {{ 3, 0}, { 4, 0}, { 5, 0}, { 6, 0}, { 7, 0}, { 8, 0}, { 9, 0}, {10, 0}, {11, 0}},
+                            {{ 3,-1}, { 3,-2}, { 3,-3}, { 3,-4}, { 3,-5}, { 3,-6}, { 3,-7}, { 3,-8}, { 3,-9}},
+                            {{ 3, 3}, { 4, 3}, { 5, 3}, { 6, 3}, { 7, 3}, { 8, 3}, { 9, 3}, {10, 3}, {11, 3}},
+                            {{ 2, 3}, { 2, 4}, { 2, 5}, { 2, 6}, { 2, 7}, { 2, 8}, { 2, 9}, { 2,10}, { 2,11}},
+                            {{-1, 2}, {-2, 2}, {-3, 2}, {-4, 2}, {-5, 2}, {-6, 2}, {-7, 2}, {-8, 2}, {-9, 2}},
+                            {{-1, 3}, {-1, 4}, {-1, 5}, {-1, 6}, {-1, 7}, {-1, 8}, {-1, 9}, {-1,10}, {-1,11}}
+                        }
+    local second_roads = {
+                        {{-1,-9}, {-2,-9}, {-3,-9}, {-4,-9}},
+                        {{-9,-2}, {-9,-3}, {-9,-4}, {-9,-5}},
+                        {{11,-1}, {11,-2}, {11,-3}, {11,-4}},
+                        {{ 4,-9}, { 5,-9}, { 6,-9}, { 7,-9}},
+                        {{11, 4}, {11, 5}, {11, 6}, {11, 7}},
+                        {{ 2,11}, { 3,11}, { 4,11}, { 5,11}},
+                        {{-9, 3}, {-9, 4}, {-9, 5}, {-9, 6}},
+                        {{-2,11}, {-3,11}, {-4,11}, {-5,11}},
+                        }
+    local third_roads = {
+                        {{-5,-9}, {-6,-9}, {-7,-9}, {-8,-9}},
+                        {{-9,-6}, {-9,-7}, {-9,-7}, {-9,-9}},
+                        {{11,-5}, {11,-6}, {11,-7}, {11,-8}},
+                        {{ 8,-9}, { 9,-9}, {10,-9}, {11,-9}},
+                        {{11, 8}, {11, 9}, {11,10}, {11,11}},
+                        {{ 6,11}, { 7,11}, { 8,11}, { 9,11}},
+                        {{-9, 7}, {-9, 8}, {-9, 9}, {-9,10}},
+                        {{-6,11}, {-7,11}, {-8,11}, {-9,11}},
+                        }
+    local do_second_road = {false,false,false,false,false,false,false,false}
+    local do_third_road = {false,false,false,false,false,false,false,false}
+    for i,unit in ipairs(GetUnits(AiPlayer())) do
+        if GetUnitVariable(unit, "Ident") == AiCityCenter() then
+            local posx = GetUnitVariable(unit, "PosX")
+            local posy = GetUnitVariable(unit, "PosY")
+            for i,road in ipairs(first_roads) do
+            do_second_road[i] = true
+            for j,pos in ipairs(road) do
+                local px = posx+pos[1]
+                local py = posy+pos[2]
+                if (px >= 0 and py >= 0 and Map.Info.MapWidth >= px and Map.Info.MapHeight >= py and
+                    (not (GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "no-building") or
+                        GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "water") or
+                        GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "coast") or
+                        GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "wall") or
+                        GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "forest") or
+                        GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "rock")))) then
+                    CreateUnit("unit-road", AiPlayer(), {px,py})
+                else
+                    do_second_road[i] = false
+                    break -- road ends here
+                end
+            end
+        end
+        if do_second then
+            for i,road in ipairs(second_roads) do
+                if do_second_road[i] then
+                    do_third_road[i] = true
+                    for j,pos in ipairs(road) do
+                        local px = posx+pos[1]
+                        local py = posy+pos[2]
+                        if (px >= 0 and py >= 0 and Map.Info.MapWidth >= px and Map.Info.MapHeight >= py and
+                            (not (GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "no-building") or
+                                    GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "water") or
+                                    GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "coast") or
+                                    GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "wall") or
+                                    GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "forest") or
+                                    GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "rock")))) then
+                            CreateUnit("unit-road", AiPlayer(), {px,py})
+                        else
+                            do_third_road[i] = false
+                            break -- road ends here
+                        end
+                    end
+                end
+            end
+            if do_third then
+                for i,road in ipairs(third_roads) do
+                    if do_third_road[i] then
+                        for j,pos in ipairs(road) do
+                            local px = posx+pos[1]
+                            local py = posy+pos[2]
+                            if (px >= 0 and py >= 0 and Map.Info.MapWidth >= px and Map.Info.MapHeight >= py and
+                                (not (GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "no-building") or
+                                        GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "water") or
+                                        GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "coast") or
+                                        GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "wall") or
+                                        GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "forest") or
+                                        GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "rock")))) then
+                                CreateUnit("unit-road", AiPlayer(), {px,py})
+                            else
+                                break -- road ends here
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+    end
+end
+
 function CreateAiLandAttack(sleep_factor, max_force)
    local end_loop_funcs = {
       function() print("Looping !") return false end,
@@ -54,74 +160,7 @@ function CreateAiLandAttack(sleep_factor, max_force)
       function() return AiWait(AiCityCenter()) end,
       function() return AiWait(AiWorker()) end, -- start hangs if nothing available
 
-      -- a function to just generate roads. this gives the ai player an advantage, but what the heck
-      function()
-         local first_roads = {
-                                  {{ 0,-1}, { 0,-2}, { 0,-3}, { 0,-4}, { 0,-5}, { 0,-6}, { 0,-7}, { 0,-8}, { 0,-9}},
-                                  {{-1,-1}, {-2,-1}, {-3,-1}, {-4,-1}, {-5,-1}, {-6,-1}, {-7,-1}, {-8,-1}, {-9,-1}},
-                                  {{ 3, 0}, { 4, 0}, { 5, 0}, { 6, 0}, { 7, 0}, { 8, 0}, { 9, 0}, {10, 0}, {11, 0}},
-                                  {{ 3,-1}, { 3,-2}, { 3,-3}, { 3,-4}, { 3,-5}, { 3,-6}, { 3,-7}, { 3,-8}, { 3,-9}},
-                                  {{ 3, 3}, { 4, 3}, { 5, 3}, { 6, 3}, { 7, 3}, { 8, 3}, { 9, 3}, {10, 3}, {11, 3}},
-                                  {{ 2, 3}, { 2, 4}, { 2, 5}, { 2, 6}, { 2, 7}, { 2, 8}, { 2, 9}, { 2,10}, { 2,11}},
-                                  {{-1, 2}, {-2, 2}, {-3, 2}, {-4, 2}, {-5, 2}, {-6, 2}, {-7, 2}, {-8, 2}, {-9, 2}},
-                                  {{-1, 3}, {-1, 4}, {-1, 5}, {-1, 6}, {-1, 7}, {-1, 8}, {-1, 9}, {-1,10}, {-1,11}}
-                              }
-         local second_roads = {
-                                {{-1,-9}, {-2,-9}, {-3,-9}, {-4,-9}},
-                                {{-9,-2}, {-9,-3}, {-9,-4}, {-9,-5}},
-                                {{11,-1}, {11,-2}, {11,-3}, {11,-4}},
-                                {{ 4,-9}, { 5,-9}, { 6,-9}, { 7,-9}},
-                                {{11, 4}, {11, 5}, {11, 6}, {11, 7}},
-                                {{ 2,11}, { 3,11}, { 4,11}, { 5,11}},
-                                {{-9, 3}, {-9, 4}, {-9, 5}, {-9, 6}},
-                                {{-2,11}, {-3,11}, {-4,11}, {-3,11}},
-                              }
-         local do_second_road = {false,false,false,false,false,false,false,false}
-         for i,unit in ipairs(GetUnits(AiPlayer())) do
-           if GetUnitVariable(unit, "Ident") == AiCityCenter() then
-             local posx = GetUnitVariable(unit, "PosX")
-             local posy = GetUnitVariable(unit, "PosY")
-             for i,road in ipairs(first_roads) do
-               do_second_road[i] = true
-               for j,pos in ipairs(road) do
-                 local px = posx+pos[1]
-                 local py = posy+pos[2]
-                 if (px >= 0 and py >= 0 and Map.Info.MapWidth >= px and Map.Info.MapHeight >= py and
-                     (not (GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "no-building") or
-                           GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "water") or
-                           GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "coast") or
-                           GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "wall") or
-                           GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "forest") or
-                           GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "rock")))) then
-                   CreateUnit("unit-road", AiPlayer(), {px,py})
-                 else
-                   do_second_road[i] = false
-                   break -- road ends here
-                 end
-               end
-             end
-             for i,road in ipairs(second_roads) do
-               if do_second_road[i] then
-                 for j,pos in ipairs(road) do
-                   local px = posx+pos[1]
-                   local py = posy+pos[2]
-                   if (px >= 0 and py >= 0 and Map.Info.MapWidth >= px and Map.Info.MapHeight >= py and
-                       (not (GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "no-building") or
-                             GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "water") or
-                             GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "coast") or
-                             GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "wall") or
-                             GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "forest") or
-                             GetTileTerrainHasFlag(posx+pos[1], posy+pos[2], "rock")))) then
-                     CreateUnit("unit-road", AiPlayer(), {px,py})
-                   else
-                     break -- road ends here
-                   end
-                 end
-               end
-             end
-           end
-         end
-      end,
+      function () return GenerateRoads(true, false) end,
 
       function() return AiSet(AiWorker(), 4) end,
 	  function() return AiWait(AiWorker()) end,
@@ -135,6 +174,8 @@ function CreateAiLandAttack(sleep_factor, max_force)
       function() return AiAttackWithForce(1) end,
       function() return AiSleep(1) end,      
 
+      function () return GenerateRoads(true, false) end,
+
       function() return AiSet(AiWorker(), 9) end,
       function() return AiSleep(500) end,
       function() return AiNeed(AiBlacksmith()) end,
@@ -147,6 +188,8 @@ function CreateAiLandAttack(sleep_factor, max_force)
       function() return AiSleep(500) end,
       function() return AiAttackWithForce(1) end,
 
+      function () return GenerateRoads(true, true) end,
+
 	  function() return AiWait(AiBlacksmith()) end,
       function() return AiResearch(AiUpgradeWeapon1()) end,
       function() return AiResearch(AiUpgradeArmor1()) end,
@@ -156,6 +199,8 @@ function CreateAiLandAttack(sleep_factor, max_force)
       function() return AiResearch(AiUpgradeMissile2()) end,
       function() return AiNeed(AiBarracks()) end,
 	  function() return AiWait(AiBarracks()) end,
+
+      function () return GenerateRoads(true, true) end,
 
       function() return AiForce(0, {AiSoldier(), 3, AiShooter(), 2}) end,
       function() return AiForce(1, {AiSoldier(), 3, AiShooter(), 1}) end,
@@ -173,11 +218,15 @@ function CreateAiLandAttack(sleep_factor, max_force)
       function() return AiSleep(500) end,
       function() return AiAttackWithForce(1) end,
 
+      function () return GenerateRoads(true, true) end,
+
       function() return AiSleep(500) end,
 
       function() return AiForce(1, {AiSoldier(), 3, AiShooter(), 1, AiCatapult(), 1}) end,
       function() return AiWaitForce(1) end,
       function() return AiAttackWithForce(1) end,
+
+      function () return GenerateRoads(true, true) end,
 
       function() return AiSleep(500) end,
       function() return AiNeed(AiStables()) end,
@@ -197,16 +246,22 @@ function CreateAiLandAttack(sleep_factor, max_force)
       function() return AiWaitForce(1) end,
       function() return AiAttackWithForce(1) end,
 
+      function () return GenerateRoads(true, true) end,
+
       function() return AiSleep(500) end,
       function() return AiNeed(AiTemple()) end,
       function() return AiResearch(AiMageSpell2()) end,
       function() return AiResearch(AiMageSpell3()) end,
+
+      function () return GenerateRoads(true, true) end,
 
       function() return AiForce(0, {AiSoldier(), 1, AiShooter(), 2, AiCavalry(), 4, AiMage(), 1, AiCatapult(), 1}) end,
       function() return AiForce(1, {AiSoldier(), 1, AiShooter(), 2, AiCavalry(), 3, AiMage(), 1, AiCatapult(), 1}) end,
       function() return AiWaitForce(0) end,
       function() return AiWaitForce(1) end,
       function() return AiAttackWithForce(1) end,
+
+      function () return GenerateRoads(true, true) end,
 
       function() return AiSleep(500) end,
       function() return AiNeed(AiMageTower()) end,
@@ -222,11 +277,15 @@ function CreateAiLandAttack(sleep_factor, max_force)
       function() return AiSleep(500) end,
       function() return AiResearch(AiSummonerSpell2()) end,
 
+      function () return GenerateRoads(true, true) end,
+
       function() return AiForce(0, {AiSoldier(), 1, AiShooter(), 2, AiCavalry(), 6, AiCatapult(), 1, AiMage(), 2, AiSummoner(), 2}) end,
       function() return AiForce(1, {AiSoldier(), 1, AiShooter(), 2, AiCavalry(), 2, AiCatapult(), 1, AiMage(), 1, AiSummoner(), 1}) end,
       function() return AiWaitForce(0) end,
       function() return AiWaitForce(1) end,
       function() return AiAttackWithForce(1) end,
+
+      function () return GenerateRoads(true, true) end,
 
       function() return AiSleep(500) end,
 
@@ -237,6 +296,8 @@ function CreateAiLandAttack(sleep_factor, max_force)
       function() return AiAttackWithForce(1) end,
 
       function() return AiSleep(500) end,
+
+      function () return GenerateRoads(true, true) end,
 
       function() return AiForce(0, {AiSoldier(), 1, AiShooter(), 2, AiCavalry(), 6, AiCatapult(), 1, AiMage(), 2, AiSummoner(), 5}) end,
       function() return AiForce(1, {AiSoldier(), 1, AiShooter(), 2, AiCavalry(), 2, AiCatapult(), 1, AiMage(), 1, AiSummoner(), 1}) end,
@@ -252,6 +313,8 @@ function CreateAiLandAttack(sleep_factor, max_force)
       function() return AiWaitForce(1) end,
       function() return AiAttackWithForce(0) end,
       function() return AiAttackWithForce(1) end,
+
+      function () return GenerateRoads(true, true) end,
 
       function() return AiSleep(500) end,
       function() return AiSet(AiWorker(), 25) end,
