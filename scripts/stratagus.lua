d30812076163e5ee31362c52bdb97f895e1e0a3a
Tim Felgentreff <timfelgentreff@gmail.com>
2018-09-29 19:23:35 +0000

    Release 2.4.2

--- scripts/stratagus.lua
+++ scripts/stratagus.lua
@@ -37,7 +37,7 @@ war1gus.Name = "War1gus"
 war1gus.Version = "2.4.2"
 war1gus.Homepage = "https://github.com/Wargus/war1gus"
 war1gus.Licence = "GPL v2"
-war1gus.Copyright = "(C) 1998-2017 by The Stratagus Project"
+war1gus.Copyright = "(c) 1998-2018 by The Stratagus Project"
 
 function file_exists(path, name)
    for i,f in ipairs(ListFilesInDirectory(path)) do
