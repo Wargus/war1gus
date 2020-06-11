033d400aaceb055316ca1dda374bae6a967e2654
Tim Felgentreff <timfelgentreff@gmail.com>
2020-06-11 21:39:00 +0200

    fix dmg formula for war1gus

--- scripts/wc1.lua
+++ scripts/wc1.lua
@@ -58,7 +58,7 @@ ShouldTogglePlayerRace = {}
 UnitEquivalents = UnitEquivalents or { }
 
 local dmg_formula = Add(AttackerVar("BasicDamage"),
-                        Sub(AttackerVar("PiercingDamage"), DefenderVar("Armor")))
+                        Max(Sub(AttackerVar("PiercingDamage"), DefenderVar("Armor")), 0))
 local dmg_with_20pc_miss = NumIf(LessThan(Rand(5), 1), 0, dmg_formula)
 -- WC1 units miss around 20% of the time, except the catapult
 SetDamageFormula(
