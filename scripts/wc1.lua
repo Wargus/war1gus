f425b7301732f5a0841807e260ea183a3856d0c9
Tim Felgentreff <timfelgentreff@gmail.com>
2020-06-28 13:25:24 +0200

    use default damage formula again

--- scripts/wc1.lua
+++ scripts/wc1.lua
@@ -60,11 +60,14 @@ UnitEquivalents = UnitEquivalents or { }
 local dmg_formula = Add(AttackerVar("BasicDamage"),
                         Max(Sub(AttackerVar("PiercingDamage"), DefenderVar("Armor")), 0))
 local dmg_with_20pc_miss = NumIf(LessThan(Rand(5), 1), 0, dmg_formula)
--- WC1 units miss around 20% of the time, except the catapult
-SetDamageFormula(
-   NumIf(Equal(AttackerVar("BasicDamage"), 255), -- 255 is the catapult dmg
-         dmg_formula,
-         dmg_with_20pc_miss))
+-- WC1 units miss around 20% of the time, except the catapult. Using this damage
+-- formula isn't quite right, I found it documented somewhere, but it doesn't
+-- work nicely.
+
+-- SetDamageFormula(
+--    NumIf(Equal(AttackerVar("BasicDamage"), 255), -- 255 is the catapult dmg
+--          dmg_formula,
+--          dmg_with_20pc_miss))
 
 -- Convert unit type to the opposite race, if current player's race
 -- does not match the predefined race
