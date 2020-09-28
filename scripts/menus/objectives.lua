a781a140f50ff6367f8a533ded6e8d030fe9d74e
Tim Felgentreff <timfelgentreff@gmail.com>
2020-09-28 13:47:15 +0200

    Show mission objectives in menu. Fixes #170

--- scripts/menus/objectives.lua
+++ scripts/menus/objectives.lua
@@ -2,11 +2,10 @@ function RunObjectivesMenu()
   local menu = WarGameMenu(panel(1))
 
   menu:addLabel("Objectives", 128, 11, Fonts["large"], true)
+  --local objectives = ""
+  --table.foreachi(Objectives, function(k,v) objectives = objectives .. v .. "\n" end)
 
-  local objectives = ""
-  table.foreachi(Objectives, function(k,v) objectives = objectives .. v .. "\n" end)
-
-  local l = MultiLineLabel(objectives)
+  local l = MultiLineLabel(objectives[1])
   l:setFont(Fonts["large"])
   l:setAlignment(MultiLineLabel.LEFT)
   l:setLineWidth(228)
